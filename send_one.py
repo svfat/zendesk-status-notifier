132d4c4cae34f19f4e197049cb847c01dd0d8b95
Stan Fateev <fateevstas@yandex.ru>
2017-09-20 19:44:13 +0200

    fix hours

--- send_one.py
+++ send_one.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from send_all import Sender, DT_FORMAT
 
 default_sender = Sender()
@@ -6,8 +8,8 @@ default_sender = Sender()
 def send_one(record):
     name = record.agent.name
     status = record.status
-    created_at = record.created_at.strftime(DT_FORMAT)
+    created_at = record.created_at - timedelta(hours=12)
     template = "{} status changed to: {} at {}"
-    msg = template.format(name, status, created_at)
+    msg = template.format(name, status, created_at.strftime(DT_FORMAT))
     default_sender._send(msg, msg)
     print(msg)
